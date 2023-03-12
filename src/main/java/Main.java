584161996870607d8e5dd39867e37c3837c3dada
Oscar992 <oscarmanuel.2810@gmail.com>
2023-03-12 16:37:20 -0500

    demas archivos

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        final var a = 4;
+        final var b = 5;
+
+        System.out.printf("La suma de los numeros %d y %d es %d", a, b, (a + b));
+    }
+}
